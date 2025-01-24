a5b7f3df4fcedf755d236b3847c5f93c9eba3144
mmaruf23 <maruuf82@gmail.com>
2025-01-25 00:19:45 +0700

    responsif bikin pusing

--- D5/mock-up/tailwind.config.js
+++ D5/mock-up/tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        plus: ["Plus Jakarta Sans", "serif"]
+      }
+    },
   },
   plugins: [],
-}
-
+};
